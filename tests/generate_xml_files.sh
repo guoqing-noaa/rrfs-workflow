6ee20b3d5702957a90c13d205024f3d78e54f83e
Guoqing Ge <guoqing.ge@noaa.gov>
2026-04-09 00:39:18 -0600

    get Tests ready (#11)
    * create tests/ and start adding files
    
    * update tests/generate_xml_files.sh
    
    * add compare_xml_outputs.yaml
    
    * temporarily disable compare_xml_outputs.yaml on PRs
    
    * address the linter comments

---
+++ tests/generate_xml_files.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2016
+rundir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wflow="${rundir}/../workflow"
+cd "${wflow}" || exit 1
+mkdir -p "${rundir}/xml"
+
+# conus12km
+myexp=conus12km
+myfile=${wflow}/exp/exp.${myexp}
+sed 's|^export OPSROOT=/scratch3/BMC/wrfruc/gge/OPSROOT/${EXP_NAME}|export OPSROOT=./OPSROOT/${EXP_NAME}|' "${myfile}" > "exp.test"
+./setup_rocoto.py "exp.test"
+mv "OPSROOT/hrly_12km/exp/rrfsdet/rrfs.xml" "${rundir}/xml/${myexp}_retro.xml"
+
+{ cat "exp.test"; echo -e 'export REALTIME=true'; } > exp.tmp
+./setup_rocoto.py exp.tmp
+mv "OPSROOT/hrly_12km/exp/rrfsdet/rrfs.xml" "${rundir}/xml/${myexp}_rt.xml"
+
+# ens_conus12km
+myexp=ens_conus12km
+myfile=${wflow}/exp/exp.${myexp}
+sed 's|^export OPSROOT=/scratch3/BMC/wrfruc/gge/OPSROOT/${EXP_NAME}|export OPSROOT=./OPSROOT/${EXP_NAME}|' "${myfile}" > "exp.test"
+./setup_rocoto.py "exp.test"
+mv "OPSROOT/hrly_12km/exp/rrfsenkf/rrfs.xml" "${rundir}/xml/${myexp}_retro.xml"
+
+{ cat "exp.test"; echo -e 'export REALTIME=true'; } > exp.tmp
+./setup_rocoto.py exp.tmp
+mv "OPSROOT/hrly_12km/exp/rrfsenkf/rrfs.xml" "${rundir}/xml/${myexp}_rt.xml"
+
+# ens_conus12km
+# conus3km
+# ens_conus3km
+# na12km
+# ens_na12km
