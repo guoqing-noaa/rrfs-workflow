2fa6a99a80e0f8daef5d58416f6a28d2ee21779d
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-04-09 22:46:13 -0600

    fix bad_examples.sh

--- ush/bad_example.sh
+++ ush/bad_example.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Sample job script with intentional RRFS norm violations for testing.
 # --- RRFS001: dot-source ---
-. /etc/profile
+source /etc/profile
 
 # --- RRFS002: single bracket ---
-if [ -d /tmp ]; then
+if [[ -d /tmp ]]; then
   echo "exists"
 fi
