ec845257199f4fa92c090d0210f9d24ba4cfc861
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-07-03 11:18:53 -0600

    use existing NSOIL_LEVELS instead of introducing a new variable NUM_SOIL_LAYERS

--- scripts/exrrfs_fcst.sh
+++ scripts/exrrfs_fcst.sh
@@ -53,7 +53,7 @@ if [[ "${DO_RTMA^^}" == "TRUE" ]]; then
 fi
 physics_suite=${PHYSICS_SUITE:-'mesoscale_reference'}
 lsm_scheme=${LSM_SCHEME:-'sf_ruc'}
-num_soil_layers=${NUM_SOIL_LAYERS:-9}
+nsoillevels=${NSOIL_LEVELS:-9}
 jedi_da=true #true
 
 pio_num_iotasks=${NODES}
